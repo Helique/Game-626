718a4bd1bdd4c2af216f385217a14439e87f4f03
coder-guy22296 <canpy30@gmail.com>
2014-07-18 21:18:21 -0700

    added a package for audio which will contain the Audio engine and store references to the audio files --Can

---
+++ JGame/src/audio/SoundClipLibrary.java
@@ -0,0 +1,5 @@
+package audio;
+
+public enum SoundClipLibrary {
+	DOOR_CREAK(/*e.g.  resources/doorCreaking.wav*/)
+}
--- JGame/src/events/Event.java
+++ JGame/src/events/Event.java
@@ -1,27 +1,37 @@
 package events;
 
+import audio.SoundClipLibrary;
+
 public class Event {
 	int eventNumber =0;
 	private EventType type = null;
 	String area;
 	int TeleportationLocation;
-	public int getTeleportationLocation() {
-		return TeleportationLocation;
-	}
+	//Event
 	public Event(EventType t){
 		this.type = t;	
-	}
-	public String getTeleArea(){
-		return area;
-	}
-	public void setTeleLocation(String area, int TeleLocation){
-		this.TeleportationLocation = TeleLocation;
-		this.area = area;
 	}
 	public EventType getType() {
 		return type;
 	}
 	public void setType(EventType type) {
 		this.type = type;
+	}
+	
+	//Teleport
+	public String getTeleArea(){
+		return area;
+	}
+	public int getTeleportationLocation() {
+		return TeleportationLocation;
+	}
+	public void setTeleLocation(String area, int TeleLocation){
+		this.TeleportationLocation = TeleLocation;
+		this.area = area;
+	}
+	
+	//sound effect
+	public void playSoundEffect(SoundClipLibrary sound){
+		
 	}
 }
